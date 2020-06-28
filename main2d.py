8f797699ee3356487625237946049ecbe3a176b5
yfq512 <1085561337@qq.com>
2020-06-28 14:41:19 +0800

    2d/3d实际调用

---
+++ main2d.py
@@ -0,0 +1,29 @@
+import face_alignment
+from skimage import io
+import cv2
+
+# 2D
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, device='cpu', flip_input=False)
+input_ = io.imread('./abc.png')
+preds = fa.get_landmarks(input_)
+img = cv2.imread('./abc.png')
+point_size = 1
+point_color = (0, 0, 255) # BGR
+thickness = 4 # 可以为 0 、4、8
+print(len(preds))
+for point in preds[0]:
+    print(point)
+    po1 = int(point[0])
+    po2 = int(point[1])
+    point_ = (po1, po2)
+    cv2.circle(img, point_, point_size, point_color, thickness)
+cv2.imwrite('abc2.png', img)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+
+# 3D
+# fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, flip_input=False)
+
+# input = io.imread('../test/assets/aflw-test.jpg')
+# preds = fa.get_landmarks(input)
---
+++ main3d.py
@@ -0,0 +1,36 @@
+import face_alignment
+from skimage import io
+import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# 2D
+# fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, device='cpu', flip_input=False)
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cpu', flip_input=False)
+input_ = io.imread('./abc.png')
+preds = fa.get_landmarks(input_)
+img = cv2.imread('./abc.png')
+point_size = 1
+point_color = (0, 0, 255) # BGR
+thickness = 4 # 可以为 0 、4、8
+print(len(preds))
+xx = []
+yy = []
+zz = []
+for point in preds[0]:
+    print(point)
+    xx.append(point[0])
+    yy.append(point[1])
+    zz.append(point[2])
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(xx, yy, zz)
+plt.show()
+#cv2.imwrite('abc2.png', img)
+
+
+# 3D
+# fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, flip_input=False)
+
+# input = io.imread('../test/assets/aflw-test.jpg')
+# preds = fa.get_landmarks(input)
